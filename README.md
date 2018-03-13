0043f1c772f7a246c890b6b6ec8293d144575dcc
Rogers <793574772@qq.com>
2018-03-13 10:24:37 +0800

    docs(whole project to use commitizen): write some for README.md and test for commitizen

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+Font  use  vue+element-ui
+Php   use  thinkphp
 
+
+2018.03.13 test to use commitizen to constraint git commit
---
+++ php/application/font/controller/Base.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\font\controller;
+
+use app\common\controller\Common;
+use think\Request;
+use think\Db;
+
+class Base extends Common
+{
+    public function login()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+
+
+}
