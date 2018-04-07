cfadf6360903e60f38eb5d962835827d2d613977
Rogers <793574772@qq.com>
2018-04-07 12:36:00 +0800

    feat(前台): 404页面

--- font/src/components/404.vue
+++ font/src/components/404.vue
@@ -1,14 +1,31 @@
 <template>
-<div>
-  404
-</div>
+  <div>
+    <img src="http://img60.ddimg.cn/upload_img/00111/404/error_01.jpg" alt="">
+    <p class="title">对不起，您要访问的页面暂时没有找到，</p>
+    <el-button type="info" @click="goToBack">返回之前页面</el-button>
+    <el-button type="info" @click="goToLogin">返回首页重新登录账号</el-button>
+  </div>
 
 </template>
 
 <script>
-  export default {}
+  export default {
+    methods: {
+      goToBack () {
+        this.$router.go(-1)
+      },
+      goToLogin () {
+        console.log(this.$route)
+        this.$router.push('/login')
+      }
+    }
+  }
 </script>
 
 <style scoped>
-
+  .title{
+    font: 14px/24px "Microsoft YaHei";
+    color: #888;
+    padding: 40px 0 15px;
+  }
 </style>
